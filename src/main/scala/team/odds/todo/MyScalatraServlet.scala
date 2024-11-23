6f9fe631768c6f218b8ba3c5635948b42f76719b
RuuF <roof@odds.team>
2024-11-23 17:27:11 +0700

    Complete crud api

--- src/main/scala/team/odds/todo/MyScalatraServlet.scala
+++ src/main/scala/team/odds/todo/MyScalatraServlet.scala
@@ -1,11 +1,36 @@
 package team.odds.todo
 
 import org.scalatra._
+import scala.collection.mutable
+
+case class Todo(id:Int, title: String)
 
 class MyScalatraServlet extends ScalatraServlet {
+  var todos: mutable.Map[Int, Todo] = mutable.Map()
 
-  get("/") {
-    views.html.hello()
+  get("/") { 
+    contentType = "text/html" 
+    s"<h1>Welcome to Todo List API</h1>" 
+  } 
+  // List all todos 
+  get("/todos") { 
+    contentType = "application/json" 
+    todos.values.toList 
+  } 
+  // Add a new todo 
+  post("/todos") { 
+    contentType = "application/json" 
+    val id: Int = (if (todos.isEmpty) 1 else todos.keys.max + 1) 
+    val description = params("description") 
+    val todo = Todo(id, description) 
+    todos += (id -> todo) 
+    todo 
+  } 
+  // Delete a todo 
+  delete("/todos/:id") { 
+    contentType = "application/json" 
+    val id = params("id").toInt 
+    todos -= id 
+    s"Todo with id $id deleted."
   }
-
 }
